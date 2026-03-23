207ac3fd28ea58ed6dfea7b8e0cfbe1a117ab22d
junsangy1234 <junasngy1234@gmail.com>
2026-03-23 16:46:26 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/memorybank/MemorybankApplication.java
@@ -0,0 +1,13 @@
+package com.memorybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorybankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorybankApplication.class, args);
+	}
+
+}
